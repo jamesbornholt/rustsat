a4db219d31dba6f0c4fd18c288309d5b03033218
James Bornholt <bornholt@cs.washington.edu>
2021-03-16 19:23:29 -0500

    refactor solver state

--- src/solver.rs
+++ src/solver.rs
@@ -3,19 +3,6 @@ use crate::SatResult;
 use log::trace;
 use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Decision {
-    literal: Literal,
-    level: DecisionLevel,
-    implied: bool,
-}
-
-impl Decision {
-    fn negated(&self) -> Literal {
-        self.literal.negated()
-    }
-}
-
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Assignment {
     True,
@@ -32,23 +19,52 @@ pub struct Solver {
 
 #[derive(Debug)]
 struct SolverState {
-    decisions: Vec<Decision>,
-    assignment: Vec<Assignment>,
+    variables: Vec<VariableState>,
+    trail: Vec<Variable>,
     decision_level: DecisionLevel,
 }
+
+#[derive(Debug, Clone)]
+struct VariableState {
+    assignment: Assignment,
+    reason: Option<ClauseIdx>,
+    decision_level: DecisionLevel,
+}
+
+impl VariableState {
+    fn clear(&mut self) {
+        self.assignment = Assignment::Undecided;
+        self.reason = None;
+        self.decision_level = DecisionLevel(0);
+    }
+}
+
+impl Default for VariableState {
+    fn default() -> Self {
+        VariableState {
+            assignment: Assignment::Undecided,
+            reason: None,
+            decision_level: DecisionLevel(0),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct ClauseIdx(usize);
 
 impl SolverState {
     fn new(formula: &Formula) -> Self {
         let num_variables = formula.num_variables();
+        let variables = vec![Default::default(); num_variables];
         Self {
-            decisions: vec![],
-            assignment: vec![Assignment::Undecided; num_variables as usize],
+            variables,
+            trail: vec![],
             decision_level: DecisionLevel(0),
         }
     }
 
     fn assignment_for(&self, literal: &Literal) -> Assignment {
-        match self.assignment[literal.idx()] {
+        match self.variables[literal.idx()].assignment {
             Assignment::True => {
                 if literal.is_positive() {
                     Assignment::True
@@ -67,29 +83,26 @@ impl SolverState {
         }
     }
 
-    fn assign(&mut self, literal: &Literal, implied: bool) {
-        assert_eq!(self.assignment[literal.idx()], Assignment::Undecided);
-        assert!(implied || self.decision_level > DecisionLevel(0));
+    fn assign(&mut self, literal: &Literal, reason: Option<ClauseIdx>) {
+        assert_eq!(self.assignment_for(literal), Assignment::Undecided);
+        assert!(reason.is_some() || self.decision_level > DecisionLevel(0));
 
         trace!(
             "{} {:?} at level {:?}",
-            if implied { "implied" } else { "decision" },
+            if reason.is_some() { "implied" } else { "decision" },
             literal,
             self.decision_level
         );
 
-        let decision = Decision {
-            literal: literal.clone(),
-            level: self.decision_level,
-            implied,
-        };
-        self.decisions.push(decision);
-
-        self.assignment[literal.idx()] = if literal.is_positive() {
+        self.trail.push(*literal.variable());
+        let var = &mut self.variables[literal.idx()];
+        var.assignment = if literal.is_positive() {
             Assignment::True
         } else {
             Assignment::False
-        }
+        };
+        var.reason = reason;
+        var.decision_level = self.decision_level;
     }
 }
 
@@ -135,7 +148,7 @@ impl Solver {
             match self.decide() {
                 None => break SatResult::Satisfiable,
                 Some(literal) => {
-                    self.state.assign(&literal, false);
+                    self.state.assign(&literal, None);
                     while self.bcp() == BcpResult::Conflict {
                         match self.analyze_conflict() {
                             None => return SatResult::Unsatisfiable,
@@ -151,7 +164,7 @@ impl Solver {
         let mut did_work = true;
         while did_work {
             did_work = false;
-            'clauses: for clause in &self.clauses {
+            'clauses: for (idx, clause) in self.clauses.iter().enumerate() {
                 let mut last_literal = None;
                 'literals: for literal in clause.literals() {
                     match self.state.assignment_for(literal) {
@@ -173,7 +186,7 @@ impl Solver {
                 // if last_literal is none, every literal was false => we have a conflict
                 // otherwise we can apply unit resolution and continue
                 match last_literal {
-                    Some(literal) => self.state.assign(literal, true),
+                    Some(literal) => self.state.assign(literal, Some(ClauseIdx(idx))),
                     None => return BcpResult::Conflict,
                 }
                 did_work = true;
@@ -186,8 +199,8 @@ impl Solver {
         // TODO a less stupid heuristic. for now, just enumerate variables looking for one that's unassigned
         // why is it complete to only return positive assignments? because [`analyze_conflict`] will generate a conflict
         // clause that will reverse this decision if it's involved in a conflict.
-        for (i, assignment) in self.state.assignment.iter().enumerate() {
-            if *assignment == Assignment::Undecided {
+        for (i, state) in self.state.variables.iter().enumerate() {
+            if state.assignment == Assignment::Undecided {
                 return Some(Literal::Positive(Variable(i)));
             }
         }
@@ -196,7 +209,8 @@ impl Solver {
 
     fn analyze_conflict(&mut self) -> Option<Backtrack> {
         // Invariant: self.bcp() returned conflict
-        // Invariant: self.state.decisions is not empty, and contains at least one non-implied decision
+        // Invariant: self.state.decisions is not empty, contains at least one non-implied decision,
+        //            and ends with at least one implied decision
 
         // TODO better conflict analysis
         // for now: the most recent decision was wrong. so we:
@@ -205,43 +219,50 @@ impl Solver {
         // (b) return the level right before that one
         // If there are no non-implied decisions, our conflict is at level 0, so we return None
         // (nowhere to backtrack to)
-        let (decision_index, last_decision) = self
+        let decision_index = self
             .state
-            .decisions
+            .trail
             .iter()
-            .enumerate()
-            .rev()
-            .find(|(_i, decision)| !decision.implied)?;
+            .rposition(|var| self.state.variables[var.0].reason.is_none())?;
+        let last_decision_variable = self.state.trail[decision_index];
+        let last_decision_level = self.state.variables[last_decision_variable.0].decision_level;
 
         // The conflict clause negates every non-implied decision up to and including the latest one
         let conflict_clause = self
             .state
-            .decisions
+            .trail
             .iter()
             .take(decision_index + 1)
-            .filter(|d| !d.implied)
-            .map(|d| d.negated());
+            .filter(|v| self.state.variables[v.0].reason.is_none())
+            .map(|v| {
+                if self.state.variables[v.0].assignment == Assignment::True {
+                    Literal::Negative(*v)
+                } else {
+                    Literal::Positive(*v)
+                }
+            });
         let conflict_clause = Clause::new(conflict_clause);
+
         trace!(
             "backtracking to {:?}, learned clause {:?}",
-            last_decision.level.0 - 1,
+            last_decision_level.0 - 1,
             conflict_clause
         );
         self.clauses.push(conflict_clause);
 
         // All non-implied decisions are above level 0, so the subtraction is safe
-        assert!(last_decision.level > DecisionLevel(0));
+        assert!(last_decision_level > DecisionLevel(0));
         Some(Backtrack {
-            level: DecisionLevel(last_decision.level.0.saturating_sub(1)),
+            level: DecisionLevel(last_decision_level.0 - 1),
             decision_index,
         })
     }
 
     fn backtrack(&mut self, backtrack: Backtrack) {
-        assert!(backtrack.decision_index < self.state.decisions.len());
-        let dropped = self.state.decisions.split_off(backtrack.decision_index);
-        for decision in &dropped {
-            self.state.assignment[decision.literal.idx()] = Assignment::Undecided;
+        assert!(backtrack.decision_index < self.state.trail.len());
+        let dropped = self.state.trail.split_off(backtrack.decision_index);
+        for variable in &dropped {
+            self.state.variables[variable.0].clear();
         }
         self.state.decision_level = backtrack.level;
     }
